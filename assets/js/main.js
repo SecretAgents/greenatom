a0a18116df9b8e6a5abbfe2607d1fb995bc4226c
Denis Lebedev <creskiv@gmail.com>
2018-12-16 20:32:22 +0300

    Main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
+  $('.owl-carousel-simple').addClass('owl-carousel owl-theme').owlCarousel({
     items: 1,
     loop: true,
     dots: true,
