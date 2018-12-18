4c3c1cc958edbad6b03bbf12a86989b50bc026a1
Nikolay Korataev <worldnillin@gmail.com>
2018-12-18 19:18:05 +0300

    news show with colorbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,16 @@ $(document).ready(function(){
     navText: ""
   });
 
+  jQuery.extend(jQuery.colorbox.settings, {
+      current: "{current} из {total}"
+      // previous: "назад",
+      // next: "вперёд",
+      // close: "закрыть",
+      // xhrError: "Не удалось загрузить содержимое.",
+      // imgError: "Не удалось загрузить изображение.",
+      // slideshowStart: "начать слайд-шоу",
+      // slideshowStop: "остановить слайд-шоу"
+ });
 
   $('.menu-open').on("click", function() {
     $('.additional-menu-wrapper').addClass('menu-change-visibility')
@@ -23,5 +33,9 @@ $(document).ready(function(){
 
   if ($('#additional-menu').size()) {
     $('#additional-menu').easytree({disableIcons: true});
+  }
+
+  if($('.gallery-list-item').size()) {
+    $('.gallery-list-item .image-gallery').colorbox({rel:'image-gallery'});
   }
 });
